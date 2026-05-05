955c4d887e5c5eb49aae0e1573b20623ac5369d6
andymongom <139020550+andymongom@users.noreply.github.com>
2026-05-05 09:21:43 -0600

    Cambio de Segundo Nivel
    Ahora es una escuela con 3 niveles. Hay más puertas y más espacio para que el enemigo te pueda perseguir. También agregué un music slider para el primer nivel.

---
+++ Dream State/Assets/Scripts/SoundEffects/MusicManager.cs
@@ -0,0 +1,45 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MusicManager : MonoBehaviour
+{
+    public static MusicManager Instance;
+
+    private AudioSource audioSource;
+    [SerializeField] private Slider musicSlider;
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+
+            audioSource = GetComponent<AudioSource>();
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    void Start()
+    {
+        if (musicSlider != null)
+        {
+            musicSlider.onValueChanged.AddListener(OnValueChanged);
+
+            SetVolume(musicSlider.value);
+        }
+    }
+
+    public void SetVolume(float volume)
+    {
+        audioSource.volume = volume;
+    }
+
+    public void OnValueChanged(float value)
+    {
+        SetVolume(value);
+    }
+}
