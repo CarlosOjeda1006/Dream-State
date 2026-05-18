072290a3bd57b6b824096b15a9e9b2265822d497
PauGarG <pau16082003@gmail.com>
2026-05-18 15:24:35 -0600

    Creditos_terminados

---
+++ Dream State/Assets/Scripts/Credits/Credits.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Credits : MonoBehaviour
+{
+    void Start()
+    {
+        Invoke("WaitToEnd", 50);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Escape))
+        {
+            SceneManager.LoadScene("MainMenu");
+        }
+    }
+
+    public void WaitToEnd()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+}
+
+
+
+
