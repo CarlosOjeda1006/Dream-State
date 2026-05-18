896c3b1f90c29442ca559508d0f734e8b89b4c11
andymongom <139020550+andymongom@users.noreply.github.com>
2026-05-18 08:06:45 -0600

    Photos
    Ya se añaden photos al menu con ciertos objetos.

--- Dream State/Assets/Scripts/Menu/Item.cs
+++ Dream State/Assets/Scripts/Menu/Item.cs
@@ -1,9 +1,16 @@
+using System.Collections;
+using TMPro;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class Item : MonoBehaviour
 {
     public GameObject itemUIPrefab;
+    public GameObject photosTab;
+    public GameObject itemPhoto;
+    public bool isPhotoItem;
+    public GameObject instructionsBox;
     public string Name;
 
 
@@ -16,11 +23,30 @@ public class Item : MonoBehaviour
             ItemPickUpUIController.Instance.ShowItemPickup(Name, itemIcon);
         }
 
+        if(isPhotoItem)
+        {
+            photosTab.SetActive(true);
+            itemPhoto.SetActive(true);
+            StartCoroutine(PickupSequence());
+        }
+
     }
 
     public void Drop(Vector3 position)
     {
         transform.position = position;
         gameObject.SetActive(true);
+    }
+    IEnumerator PickupSequence()
+    {
+        instructionsBox.SetActive(true);
+
+        instructionsBox.GetComponent<TMP_Text>().text = "New Photo Added [Press TAB to view].";
+
+        SoundEffectManager.Play("Clue");
+
+        yield return new WaitForSeconds(2f);
+
+        instructionsBox.SetActive(false);
     }
 }
