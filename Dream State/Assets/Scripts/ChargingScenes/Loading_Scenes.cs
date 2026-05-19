bac228a45162dfe69f723a621742e10498a942ca
PauGarG <pau16082003@gmail.com>
2026-05-18 18:25:25 -0600

    Escenas_Loading

---
+++ Dream State/Assets/Scripts/ChargingScenes/Loading_Scenes.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Loading_Scenes : MonoBehaviour
+{
+    public GameObject panelLoading;
+    public Slider barraCarga;
+
+    void Start()
+    {
+        panelLoading.SetActive(false);
+    }
+
+    public void NuevoJuego()
+    {
+        StartCoroutine(CargarAsync("Tutorial"));
+    }
+
+    IEnumerator CargarAsync(string nombreEscena)
+    {
+        panelLoading.SetActive(true);
+
+        AsyncOperation operacion =
+            SceneManager.LoadSceneAsync(nombreEscena);
+
+        while (!operacion.isDone)
+        {
+            float progreso =
+                Mathf.Clamp01(operacion.progress / 0.9f);
+
+            barraCarga.value = progreso;
+
+            yield return null;
+        }
+    }
+}
+
+
