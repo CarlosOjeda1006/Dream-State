6d17fad546c0ffd1308258b6fbad10dc3530354b
AnaP-310 <0253781@up.edu.mx>
2026-05-21 16:22:57 -0600

    ControlesMandospt6
    Se puede salir de imagen/pista y regresar a inventario
    
    Checar Settings+sliders

--- Dream State/Assets/Scripts/Menu/NoteViewer.cs
+++ Dream State/Assets/Scripts/Menu/NoteViewer.cs
@@ -8,19 +8,24 @@ public class NoteViewer : MonoBehaviour
     public Image bigImage;
 
     public GameObject firstButton;
-
+    public GameObject previousSelected;
     public void OpenNote(Sprite noteSprite)
     {
         SoundEffectManager.Play("Inventory_Open");
+        previousSelected = EventSystem.current.currentSelectedGameObject;
+
         viewerPanel.SetActive(true);
         EventSystem.current.SetSelectedGameObject(firstButton);
 
         bigImage.sprite = noteSprite;
+
     }
 
     public void CloseNote()
     {
         SoundEffectManager.Play("SwitchTab");
         viewerPanel.SetActive(false);
+
+        EventSystem.current.SetSelectedGameObject(previousSelected);
     }
 }
