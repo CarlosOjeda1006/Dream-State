0b6933c91eba9356063f1c3f297d609cf2820482
AnaP-310 <0253781@up.edu.mx>
2026-05-26 17:31:28 -0600

    MandoSettings
    Ya puedes acceder a los settings y salir con mando

--- Dream State/Assets/Scripts/MainMenu/PlayMenus.cs
+++ Dream State/Assets/Scripts/MainMenu/PlayMenus.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.SceneManagement;
 
 public class PlayMenus : MonoBehaviour
@@ -10,16 +11,21 @@ public class PlayMenus : MonoBehaviour
     [SerializeField] private GameObject buttonExit;
     [SerializeField] private GameObject menuSettings;
 
+    public GameObject firstButtonMenu;
+    public GameObject firstButtonSettings;
     public void Options()
     {
         //Time.timeScale = 0f;
         mainMenu.SetActive(false);
         menuSettings.SetActive(true);
+        EventSystem.current.SetSelectedGameObject(firstButtonSettings);
+
     }
     public void ExitOptions()
     {
         //Time.timeScale = 0f;
         mainMenu.SetActive(true);
+        EventSystem.current.SetSelectedGameObject(firstButtonMenu);
         menuSettings.SetActive(false);
     }
 }
